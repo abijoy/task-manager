4d041e796525cd8f8c5854de45a66190113ddd3e
Md Al Amin <mdalaminbijoy3@gmail.com>
2023-12-23 20:27:06 +0600

    added custom user registration form to add email so that we can send password reset link

--- users/views.py
+++ users/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.urls import reverse 
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-
+from .forms import CustomUserCreationForm
 # Create your views here.
 def dashboard(request):
     return render(request, 'users/dashboard.html')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('tasks:index')
         return render(request, 'users/register.html', {'form': form})
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
