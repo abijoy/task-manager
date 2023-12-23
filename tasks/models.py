f2a69cef0977c71ef23d3dcf0258d29958cc2b73
Md Al Amin <mdalaminbijoy3@gmail.com>
2023-12-23 21:13:00 +0600

    Tasks are ordered by priority and registered in admin site

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+PRIORITIES = [
+    ('LOW', 'Low'),
+    ('MEDIUM', 'Medium'),  
+    ('HIGH', 'High'),
+]
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    # TODO: add photos field later
+    priority = models.CharField(max_length=10, choices=PRIORITIES, default='LOW') 
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('priority', 'due_date')
+
+    def __str__(self):
+        return self.title
