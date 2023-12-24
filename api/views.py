680c3b62e1ff507dacf2425794b32f5e1ef08a9c
Md Al Amin <mdalaminbijoy3@gmail.com>
2023-12-24 13:11:28 +0600

    added right permissions

--- api/views.py
+++ api/views.py
@@ -3,12 +3,17 @@ from rest_framework.permissions import IsAuthenticated
 from tasks.models import Task
 
 from .serializers import TaskSerializer
-
+from .permissions import IsOwnerOnly
 
 class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+    # queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOnly]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+
 
 
 # Create your views here.
