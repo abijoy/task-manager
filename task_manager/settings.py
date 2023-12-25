de432dece50014a494786786c2d327d2b941d3d2
Md Al Amin <mdalaminbijoy3@gmail.com>
2023-12-25 14:14:55 +0600

    provided a sample .env.example and modified settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -154,8 +154,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'tasks:tasks-all'
 LOGOUT_REDIRECT_URL = 'tasks:tasks-all'
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
